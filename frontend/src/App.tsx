f3f19f07329b37a66bac50a4edea376c6296fd53
Praveenkumar <praveenkumar.b@xobin.com>
2025-12-09 09:01:59 +0530

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import LayoutShell from './components/LayoutShell';
 import WhatsAppIntegration from './components/WhatsAppIntegration';
 import Simulate from './pages/Simulate';
 import Graph from './pages/Graph';
+import CrusherSimulation from './pages/CrusherSimulation';
 
 const AppContent: React.FC = () => {
   return (
@@ -17,6 +18,7 @@ const AppContent: React.FC = () => {
   <Route path="/" element={<Simulate />} />
         <Route path="/whatsapp" element={<WhatsAppIntegration />} />
         <Route path="/simulate" element={<Simulate />} />
+  <Route path="/crusher-simulation" element={<CrusherSimulation />} />
         <Route path="/graph" element={<Graph />} />
         {/* Keep dashboards accessible if needed */}
         <Route path="/user" element={<UserDashboard />} />
